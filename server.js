0139e2032e420c35f29651e0c5d007e60d094469
Anton Veldanov <anton.veldanov@gmail.com>
2019-03-02 18:33:17 -0800

    jwt

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
 const app = express();
+
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }
+))
+
 
 //DB config
 const db = require('./config/key').mongoURI;
 
 //Connest to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDb Connected'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello!!!'));
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
