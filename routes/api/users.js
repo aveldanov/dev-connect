8a6394ab6c5fc351a48a425316556f7f554c0561
Anton Veldanov <anton.veldanov@gmail.com>
2019-03-07 06:17:56 -0800

    jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//Load input validation
+const validateRegisterInput = require('../../validation/register');
 
 //Load User model
 const User = require('../../models/User');
@@ -23,10 +25,20 @@ router.get('/test', (req, res) => res.json({ msg: "Users works" }));
 //@access   Public
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //console.log(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
+      errors.email = 'Email already exists';
+
       if (user) {
-        return res.status(400).json({ email: 'Email already exists' })
+        return res.status(400).json(errors)
       } else {
         const avatar = gravatar.url(req.body.email, {
           s: '200', //size
--- validation/register.js
+++ validation/register.js
@@ -1,12 +1,28 @@
 const Validator = require('validator');
+const isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.name = !isEmpty(data.name) ? data.name : '';
+
+
+
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
   }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required"
+  }
+
+
   return {
     errors,
-    isValid: errors
+    isValid: isEmpty(errors)
   }
 }
